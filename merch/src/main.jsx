4439c7cc0f8cff5faf92864017ae5993d402f715
Lera <lerayokovenko098@gmail.com>
2024-11-18 15:49:57 +0200

    sgsdfsdfd

--- merch/src/main.jsx
+++ merch/src/main.jsx
@@ -3,21 +3,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './output.css'
-import Discount from './components/Discount';
-import MainBlock from './components/MainBlock';
-import OurAdvantages from './components/AdvantagesBlock';
 import Header from './components/Header';
-import Discount from './components/Discount';
+import MainBlock from './components/MainBlock';
 import TShirtCard from './components/TShirtCard';
 import TShirtCarousel from './components/TShirtCarousel';
+import Discount from './components/Discount';
+import OurAdvantages from './components/AdvantagesBlock';
+
  
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
     <MainBlock />
+    <TShirtCard />
+    <TShirtCarousel />
     <Discount />
     <OurAdvantages />
-    <TShirtCard/>
-    <TShirtCarousel/>
   </StrictMode>,
 );
