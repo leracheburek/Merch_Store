6908ab2677adec6e021957a502e12f23858fb689
c <vladcharivnik@gmail.com>
2024-11-30 18:49:01 +0000

    Add minibutton and charts + statistic
    make for adminPanel

--- merch/src/components/Admin/index.jsx
+++ merch/src/components/Admin/index.jsx
@@ -1,15 +1,18 @@
 import './style.css';
 import Nav from './Nav'
 import TemporaryDrawer from './SideBar';
-
+import BasicCard from './charts'
+import BarsDataset from './stats/CardStats'
+import BasicPie from './BarChart'
 
 export default function Discount() {
   return (
     <div>
         <Nav />
         <TemporaryDrawer />
-
-        
+        <BasicCard />
+        <BarsDataset /> 
+        <BasicPie />
 
     </div>
   );
