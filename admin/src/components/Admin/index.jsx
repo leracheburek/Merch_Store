506948b9cc44b1d49583b73ad50d3ef0b6b0d149
eJwtzLtuwjAUgOF3ObMHCIFgzwwtS6NeZsuXYzB2YvfYAVLEu1dCWX/p+x+gjMFSvlPAEQTsOuTILaoOudu0W7OxetWu163d8wY7jd3ebFc7Bwwypau3SAt9LCdZl9XpnGS408W74yX0v2741H9vfuYf9evnvTn2Wjf94VoPwOBFZJ0zggCNipCAQTHpFZQd/CjypKM3MuDMCJUViU6MMCc2FSSBg/KR3RIFF9MNGOA9e8IiVQUxTjE+n/8uU1GA <vladcharivnik@gmail.com>
2024-12-04 11:28:43 +0000

    Fixed react-router-dom + fixed blocks

--- admin/src/components/Admin/index.jsx
+++ admin/src/components/Admin/index.jsx
@@ -1,29 +1,29 @@
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './style.css';
 import TemporaryDrawer from '../Admin/SideBar/index';
+
 import BarsDataset from './stats/CardStats'
 import BasicCard from './pages/Chart';
 import BasicPie from './pages/BarChart';
-
+// import BasicCard from './pages/Chart';
+import Nav from './Nav'; 
 
 export default function Admin() {
   return (
     <Router>
       <div>
+        <Nav />
         <TemporaryDrawer />
-        <BasicCard />
-        <BarsDataset /> 
-        <BasicPie />
     </div>
+
         <div className="content">
           <Routes>
             <Route path="/charts" element={<BasicCard />} />
-            {/* <Route path="/stats" element={<BarsDataset />} /> */}
             <Route path="/pie" element={<BasicPie />} />
-            {/* Додайте інші маршрути за потреби */}
+
           </Routes>
         </div>
     </Router>
-
   );
 }
