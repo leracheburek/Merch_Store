932601e3aa65066b8642ccd33e6f020e42293f44
Nick <devoleper001@gmail.con>
2024-12-05 16:42:54 +0200

    change chart

--- admin/src/components/Admin/index.jsx
+++ admin/src/components/Admin/index.jsx
@@ -1,15 +1,5 @@
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './style.css';
-<<<<<<< HEAD
-// import TemporaryDrawer from '../Admin/SideBar/index';
-// import BarsDataset from './stats/CardStats'
-=======
-import TemporaryDrawer from '../Admin/SideBar/index';
-<<<<<<< Updated upstream
-=======
->>>>>>> origin/dev
->>>>>>> Stashed changes
 import BasicCard from './pages/Chart';
 import BasicPie from './pages/BarChart';
 import Nav from './Nav'; 
