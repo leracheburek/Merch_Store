229b9d9f24e37a2fb67dff3c18ace8a8aec05591
eJwtzLtuwjAUgOF3ObMHCIFgzwwtS6NeZsuXYzB2YvfYAVLEu1dCWX/p+x+gjMFSvlPAEQTsOuTILaoOudu0W7OxetWu163d8wY7jd3ebFc7Bwwypau3SAt9LCdZl9XpnGS408W74yX0v2741H9vfuYf9evnvTn2Wjf94VoPwOBFZJ0zggCNipCAQTHpFZQd/CjypKM3MuDMCJUViU6MMCc2FSSBg/KR3RIFF9MNGOA9e8IiVQUxTjE+n/8uU1GA <vladcharivnik@gmail.com>
2024-12-05 11:56:22 +0000

    Auto stash before merge of "dev" and "origin/dev"

--- admin/src/components/Admin/index.jsx
+++ admin/src/components/Admin/index.jsx
@@ -1,7 +1,15 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './style.css';
+<<<<<<< HEAD
+// import TemporaryDrawer from '../Admin/SideBar/index';
+// import BarsDataset from './stats/CardStats'
+=======
 import TemporaryDrawer from '../Admin/SideBar/index';
+<<<<<<< Updated upstream
+=======
+>>>>>>> origin/dev
+>>>>>>> Stashed changes
 import BasicCard from './pages/Chart';
 import BasicPie from './pages/BarChart';
 import Nav from './Nav'; 
@@ -11,14 +19,12 @@ export default function Admin() {
     <Router>
       <div>
         <Nav />
-        <TemporaryDrawer />
     </div>
 
         <div className="content">
           <Routes>
             <Route path="/charts" element={<BasicCard />} />
             <Route path="/pie" element={<BasicPie />} />
-
           </Routes>
         </div>
     </Router>
