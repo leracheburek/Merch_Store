0be9a53d24e7c3630ce43e1ca50450b4d5414bbd
Nick <devoleper001@gmail.con>
2024-12-09 17:59:29 +0200

    k

--- admin/src/components/Admin/pages/Chart.jsx
+++ admin/src/components/Admin/pages/Chart.jsx
@@ -1,7 +1,5 @@
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
-import { useState } from 'react';
-
 function Chart() {
   /* ... */
   return <DndProvider backend={HTML5Backend}>...</DndProvider>
